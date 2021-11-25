1200bc1d4afb03b7b5cf0bdf65936736e41c0a12
merthanmerter <merthanm@gmail.com>
2021-11-25 07:01:18 +0300

    main

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,8 @@
-// Opera 8.0+
-var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-
-// Firefox 1.0+
-var isFirefox = typeof InstallTrigger !== 'undefined';
-
-// Internet Explorer 6-11
-var isIE = /*@cc_on!@*/ false || !!document.documentMode;
-
-
-
 window.addEventListener("DOMContentLoaded", () => {
 
     // window.location.href = window.location.href
 
     mobileMenuState = false
-
-    if (isFirefox || isIE) {
-
-        document.getElementsByName('fixMozBlurNav')[0].classList.replace('bg-opacity-75', 'bg-opacity-100')
-    }
 
     const btn = document.getElementById("toggler");
     const btnStyle = document.getElementById("togglerStyle");
@@ -28,18 +12,12 @@ window.addEventListener("DOMContentLoaded", () => {
 
         if (!mobileMenuState) {
             
-            // menu.classList.toggle('scale-y-0')
-            // menu.classList.toggle('text-white')  
-            // menu.classList.remove('max-h-0')
             menu.classList.toggle('scale-y-0')
             mobileMenuState = true
             btnStyle.setAttribute('d', 'M6 18L18 6M6 6l12 12')
 
         } else {
 
-            // menu.classList.toggle('scale-y-0')
-            // menu.classList.toggle('text-white')
-            // menu.classList.add('max-h-0')
             menu.classList.toggle('scale-y-0')
             btnStyle.setAttribute('d', 'M4 6h16M4 12h16M4 18h16')
             mobileMenuState = false
--- tailwind.config.js
+++ tailwind.config.js
@@ -33,6 +33,16 @@ module.exports = {
       colors: {
         'drs-blue': '#13a0e5',
       },
+      screens: {
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
       // keyframes: {
       //   wiggle: {
       //     '0%, 100%': {
