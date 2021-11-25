a503d44909fe703a6e04e034548649a072beb485
merthanmerter <merthanm@gmail.com>
2021-11-25 10:03:30 +0300

    main

--- src/js/app.js
+++ src/js/app.js
@@ -11,7 +11,7 @@ window.addEventListener("DOMContentLoaded", () => {
     btn.addEventListener("click", () => {
 
         if (!mobileMenuState) {
-            
+
             menu.classList.toggle('scale-y-0')
             mobileMenuState = true
             btnStyle.setAttribute('d', 'M6 18L18 6M6 6l12 12')
@@ -21,11 +21,29 @@ window.addEventListener("DOMContentLoaded", () => {
             menu.classList.toggle('scale-y-0')
             btnStyle.setAttribute('d', 'M4 6h16M4 12h16M4 18h16')
             mobileMenuState = false
-            
+
         }
 
     });
 
+    accordion = document.querySelectorAll('#accordion')
+    arrowLeft = "M15 19l-7-7 7-7"
+    arrowDown = "M19 9l-7 7-7-7"
+
+    accordion.forEach(el => {
+        el.parentNode.childNodes[1].addEventListener("click", () => {
+            if (!el.parentNode.childNodes[3].event) {
+                el.childNodes[1].childNodes[1].setAttribute('d', arrowDown)
+                el.parentNode.childNodes[3].classList.toggle('hidden')
+                el.parentNode.childNodes[3].event = true
+            } else {
+                el.childNodes[1].childNodes[1].setAttribute('d', arrowLeft)
+                el.parentNode.childNodes[3].classList.toggle('hidden')
+                el.parentNode.childNodes[3].event = false
+            }
+        })
+    })
+
     require('@themesberg/flowbite')
-    
+
 })
