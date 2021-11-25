8e0f605533012420fc3429dab1f5b3ae7360e45d
merthanmerter <merthanm@gmail.com>
2021-11-25 05:32:01 +0300

    main

--- src/js/app.js
+++ src/js/app.js
@@ -28,15 +28,19 @@ window.addEventListener("DOMContentLoaded", () => {
 
         if (!mobileMenuState) {
             
-            menu.classList.toggle('scale-y-0')
-            menu.classList.toggle('text-white')
+            // menu.classList.toggle('scale-y-0')
+            // menu.classList.toggle('text-white')
+            menu.classList.remove('max-h-0')
+            menu.classList.add('max-h-screen')
             mobileMenuState = true
             btnStyle.setAttribute('d', 'M6 18L18 6M6 6l12 12')
 
         } else {
 
-            menu.classList.toggle('scale-y-0')
-            menu.classList.toggle('text-white')
+            // menu.classList.toggle('scale-y-0')
+            // menu.classList.toggle('text-white')
+            menu.classList.add('max-h-0')
+            menu.classList.remove('max-h-screen')
             btnStyle.setAttribute('d', 'M4 6h16M4 12h16M4 18h16')
             mobileMenuState = false
             
